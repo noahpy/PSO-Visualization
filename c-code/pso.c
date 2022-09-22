d859f71e2116498aae6626ff5721fd7a594501ce
Arne <ge47com@mytum.de>
2022-09-22 17:21:08 +0200

    array of all functions

--- c-code/pso.c
+++ c-code/pso.c
@@ -3,21 +3,20 @@
 #include <stdio.h>
 #include "functions.c"
 
+typedef float(*fit_func) (float*, size_t);
+
+fit_func fit_funcs[] = {
+    sphere, ros, rast, schwefel, pizza};
+const char fit_func_amount = 5;
+
 // gets a two dimensional array of floats with the size n * dim 
 // id identifies which fittness function should be used
 
-void start_pso(int n, int dim, float** particles, float** particle_vels, int id){
-	float (*fittness_function)(float*, size_t);
-	float lowerbound, higherbound;
-	switch(id){
-		case 0:
-			fittness_function = *sphere;
-			break;
-		default:
-			return;
-		}
-
-
+void start_pso(int n, int dim, float** particles, float** particle_vels, int func_id){
+    if (func_id < 0 || func_id >= fit_func_amount) {
+        fprintf(stderr, "Error: invalid func_id");
+        return;
+    }
 	void* tmp = malloc(n*dim*sizeof(float));
 	if(tmp == NULL){
 		exit(69);

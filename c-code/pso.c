3ec9fef03e8420f4f8dbf5ce62d25502657adecb
noahs <ge42joq@mytum.de>
2022-09-22 12:00:08 +0200

    pso.c template

---
+++ c-code/pso.c
@@ -0,0 +1,27 @@
+#include <stddef.h>
+
+// gets a two dimensional array of floats with the size n * dim 
+
+void start_pso(int n, int dim, float particles[n][dim]){
+	float particle_vels[n][dim];
+
+}
+
+
+void run_pso(){
+
+}
+
+
+void update_velocity(){
+
+}
+
+void update_position(){
+
+}
+
+
+void evaluate_fittness(){
+
+}

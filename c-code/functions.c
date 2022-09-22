f9bac7f52111274cedd4a949473ada02ab8ade78
Arne <ge47com@mytum.de>
2022-09-22 12:00:07 +0200

    Rastrigin function + math library in Makefile

--- c-code/functions.c
+++ c-code/functions.c
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include <math.h>
 
 float sphere(float* x, size_t n) {
     float result = 0.0f;
@@ -14,6 +15,14 @@ float ros(float* x, size_t n) {
     for(size_t i=0; i<n-1; i++) {
         exp1 = x[i+1] - x[i]*x[i];
         result += 100 * (exp1*exp1) + (1-x[i])*(1-x[i]);
+    }
+    return result;
+}
+
+float rast(float* x, size_t n) {
+    float result = 10*n;
+    for (size_t i=0; i<n; i++) {
+        result += x[i]*x[i] - 10 * cosf(M_2_PI*x[i]);
     }
     return result;
 }

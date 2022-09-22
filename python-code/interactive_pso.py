c57677866c2c86aa6ffcbcf6ab0c45129c1939c0
noahs <ge42joq@mytum.de>
2022-09-22 17:39:30 +0200

    blah blah blah

--- python-code/interactive_pso.py
+++ python-code/interactive_pso.py
@@ -7,13 +7,14 @@ from ctypes import *
 
 
 c_pso = CDLL("../c-code/pso.so")
-c_pso.run_iter.argtypes = [c_size_t, c_size_t, POINTER(c_float), POINTER(c_float), c_int]
+c_pso.run_iter.argtypes = [c_size_t, c_size_t, POINTER(c_float), POINTER(c_float), c_int, POINTER(c_float)]
 c_pso.run_iter.restype = None
 
 c_particles = (c_float * 10)()
 c_vels = (c_float * 10)()
+c_best_pos = (c_float * 2)()
 
-c_pso.run_iter(5, 2, c_particles, c_vels, 0)
+c_pso.run_iter(5, 2, c_particles, c_vels, 0, c_best_pos)
 
 for i in range(10):
     print(c_particles[i])

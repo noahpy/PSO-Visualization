eb0d4ceaf6d97083eacd1f630f1c44c1f0a14d00
noahs <ge42joq@mytum.de>
2022-09-22 17:00:14 +0200

    completed interface

--- c-code/pso.c
+++ c-code/pso.c
@@ -1,16 +1,37 @@
 #include <stddef.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include "functions.c"
 
 // gets a two dimensional array of floats with the size n * dim 
 // id identifies which fittness function should be used
 
-void start_pso(int n, int dim, float particles[n][dim], float particle_vels[n][dim], int id){
-	for(int i=0; i<n; i++){
-		for(int j=0; j<dim; j++){
-			printf("%9.6f\n",particles[i][j]);
+void start_pso(int n, int dim, float** particles, float** particle_vels, int id){
+	float (*fittness_function)(float*, size_t);
+	float lowerbound, higherbound;
+	switch(id){
+		case 0:
+			fittness_function = *sphere;
+			break;
+		default:
+			return;
 		}
+
+
+	void* tmp = malloc(n*dim*sizeof(float));
+	if(tmp == NULL){
+		exit(69);
 	}
+	float* p = (float*) tmp;
+	for(int i=0; i<n*dim; i++){
+		p[i] = 3.1415926;
+	}
+	*particles = p;
+}
+
+void stop_pso(float* particles, float* vels){
+	free(particles);
+	free(vels);
 }
 
 
--- python-code/interactive_pso.py
+++ python-code/interactive_pso.py
@@ -1,5 +1,24 @@
 
 
-
-
 #this is a interactive GUI for PSO Visualization and Analysis.
+
+import random as r
+from ctypes import *
+
+
+c_pso = CDLL("../c-code/pso.so")
+c_pso.start_pso.argtypes = [c_int, c_int, POINTER(POINTER(c_float)), POINTER(POINTER(c_float)), c_int]
+c_pso.start_pso.restype = None
+c_pso.stop_pso.argtypes = [POINTER(c_float), POINTER(c_float)]
+c_pso.stop_pso.restype = None
+
+c_particles = POINTER(c_float)()
+c_vels = POINTER(c_float)()
+
+c_pso.start_pso(5, 2, c_particles, c_vels, 0)
+
+for i in range(10):
+    print(c_particles[i])
+
+c_pso.stop_pso(c_particles, c_vels)
+

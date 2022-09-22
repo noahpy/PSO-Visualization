4c4cce1dc4b6134de24694b30c992900081fa2f2
noahs <ge42joq@mytum.de>
2022-09-22 12:12:53 +0200

    changed make and included functions.c in pso.c

--- c-code/pso.c
+++ c-code/pso.c
@@ -1,11 +1,16 @@
 #include <stddef.h>
+#include <stdio.h>
+#include "functions.c"
 
 // gets a two dimensional array of floats with the size n * dim 
 // id identifies which fittness function should be used
 
 void start_pso(int n, int dim, float particles[n][dim], float particle_vels[n][dim], int id){
-	
-
+	for(int i=0; i<n; i++){
+		for(int j=0; j<dim; j++){
+			printf("%9.6f\n",particles[i][j]);
+		}
+	}
 }
 
 

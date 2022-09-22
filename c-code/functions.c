c7f3be3ab53dce75658d5046988672e4cbd289a3
Arne <ge47com@mytum.de>
2022-09-22 11:50:29 +0200

    rosenbrock function

--- c-code/functions.c
+++ c-code/functions.c
@@ -2,10 +2,19 @@
 
 float sphere(float* x, size_t n) {
     float result = 0.0f;
-    for (size_t i; i<n; i++) {
+    for(size_t i=0; i<n; i++) {
         result += x[i]*x[i];
     }
     return result;
 }
 
+float ros(float* x, size_t n) {
+    float result = 0.0f;
+    float exp1;
+    for(size_t i=0; i<n-1; i++) {
+        exp1 = x[i+1] - x[i]*x[i];
+        result += 100 * (exp1*exp1) + (1-x[i])*(1-x[i]);
+    }
+    return result;
+}
 

705e7d5e1c966cc2f5594fbb278467d7f62328d6
noahs <ge42joq@mytum.de>
2022-09-22 12:05:59 +0200

    velocity array as additional param

--- c-code/pso.c
+++ c-code/pso.c
@@ -1,20 +1,16 @@
 #include <stddef.h>
 
 // gets a two dimensional array of floats with the size n * dim 
+// id identifies which fittness function should be used
 
-void start_pso(int n, int dim, float particles[n][dim]){
-	float particle_vels[n][dim];
-
-}
-
-
-void run_pso(){
+void start_pso(int n, int dim, float particles[n][dim], float particle_vels[n][dim], int id){
+	
 
 }
 
 
 void update_velocity(){
-
+	
 }
 
 void update_position(){

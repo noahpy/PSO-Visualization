9214abaafb08fa28034eeee8ea0c3c40ff8b77f5
Noah Schlenker <ge42joq@mytum.de>
2022-09-28 18:19:16 +0200

    added missing parameters

--- c-code/pso.c
+++ c-code/pso.c
@@ -12,9 +12,15 @@ const char fit_func_amount = 5;
 // gets a two dimensional array of floats with the size n * dim 
 // id identifies which fittness function should be used
 
-void run_iter(size_t n, size_t dim, float* particles, float* particle_vels, int func_id, float* best_pos){
-	for(size_t i=0; i<n*dim; i++){
-		printf("%9.6f\n",particles[i]);
+void run_iter(size_t n, size_t dim, float* particles, float* particle_vels, int func_id, float* best_pos, float* pers_best_list, float vel_w, float soz_w, float cog_w){
+	size_t arr_size = n*dim;
+	float* pers_best_cpy = malloc(arr_size*4);
+	if (pers_best_cpy == NULL){
+		return;
+	}
+	memcpy(pers_best_cpy, pers_best_list, arr_size*4);
+	for(size_t i=0; i<arr_size; i++){
+		
 	}
 }
 
--- python-code/interactive_pso.py
+++ python-code/interactive_pso.py
@@ -8,16 +8,21 @@ from ctypes import *
 
 particle_amount = 5
 dimensions = 2
+vel_w = 0.5
+cog_w = 1
+soz_w = 1
 
 c_pso = CDLL("./c-code/pso.so")
-c_pso.run_iter.argtypes = [c_size_t, c_size_t, POINTER(c_float), POINTER(c_float), c_int, POINTER(c_float)]
+c_pso.run_iter.argtypes = [c_size_t, c_size_t, POINTER(c_float), POINTER(c_float), c_int, POINTER(c_float), c_float, c_float, c_float]
 c_pso.run_iter.restype = None
 
 c_particles = (c_float * (particle_amount*dimensions))()
 c_vels = (c_float * (particle_amount*dimensions))()
+c_pers_bests = (c_float * (particle_amount*dimensions))()
 c_best_pos = (c_float * dimensions)()
 
-c_pso.run_iter(particle_amount, dimensions, c_particles, c_vels, 0, c_best_pos)
+
+c_pso.run_iter(particle_amount, dimensions, c_particles, c_vels, 0, c_best_pos, c_pers_bests, vel_w, soz_w, cog_w)
 
 for i in range(particle_amount*dimensions):
     #print(c_particles[i])
